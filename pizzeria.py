439449583f44930c2830ebe0719a001c33c10730
AlexStudent <sanchofeed@gmail.com>
2018-11-13 12:19:51 +0200

    Added pizzeria class.

---
+++ pizzeria.py
@@ -0,0 +1,32 @@
+import datetime
+
+
+class Pizzeria(object):
+    def __init__(self, title, city, default_pizzas):
+        self.__title = title
+        self.__city = city
+        self.__current_day = datetime.datetime.now().weekday()
+        self.__default_pizzas = default_pizzas
+
+    @property
+    def title(self):
+        return self.__title
+
+    @title.setter
+    def city(self, title):
+        self.__title = title
+
+    @property
+    def city(self):
+        return self.__city
+
+    @city.setter
+    def city(self, city):
+        self.__city = city
+
+    @property
+    def current_day(self):
+        return self.__current_day
+
+    def get_pizza_of_the_day(self):
+        return self.__default_pizzas[self.__current_day]
