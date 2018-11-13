7bfd519802a734b60c5761b0e29b963d159491d4
AlexStudent <sanchofeed@gmail.com>
2018-11-13 11:48:23 +0200

    Defined client and order classes with some properties.

--- client.py
+++ client.py
@@ -1,1 +1,29 @@
-# check
+class Client(object):
+    def __init__(self, f_name, s_name, city):
+        self.__first_name = f_name
+        self.__second_name = s_name
+        self.__city = city
+
+    @property
+    def first_name(self):
+        return self.__firstName
+
+    @first_name.setter
+    def first_name(self, f_name):
+        self.__first_name = f_name
+
+    @property
+    def second_name(self):
+        return self.__second_name
+
+    @second_name.setter
+    def second_name(self, s_name):
+        self.__second_name = s_name
+
+    @property
+    def city(self):
+        return self.__city
+
+    @city.setter
+    def city(self, city):
+        self.__city = city
--- order.py
+++ order.py
@@ -1,1 +1,17 @@
-# check
+class Order(object):
+    def __init__(self, client, pizza, ingredients):
+        self.__client = client
+        self.__pizza = pizza
+        self.__ingredients = ingredients
+
+    @property
+    def client(self):
+        return self.__client
+
+    @property
+    def pizza(self):
+        return self.__pizza
+
+    @property
+    def ingredients(self):
+        return self.__ingredients
