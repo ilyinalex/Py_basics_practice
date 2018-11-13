f12bf549522806571223a721cfe62eb36f313635
AlexStudent <sanchofeed@gmail.com>
2018-11-13 12:40:16 +0200

    Added pizza system class.

---
+++ pizza_system.py
@@ -0,0 +1,19 @@
+class PizzaSystem(object):
+    __instance = None
+
+    @staticmethod
+    def get_instance():
+        if PizzaSystem.__instance is None:
+            PizzaSystem()
+        return PizzaSystem.__instance
+
+    def __init__(self, pizzerias):
+        if PizzaSystem.__instance is None:
+            PizzaSystem.__instance = self
+            self.__pizzerias = pizzerias
+
+    def add_pizzeria(self, pizzeria):
+        self.__pizzerias.append(pizzeria)
+
+    def rm_pizzeria(self, pizzeria):
+        self.__pizzerias.remove(pizzeria)
