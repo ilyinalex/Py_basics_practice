29273b5d276aca7cba3e407489a13c21c51baa51
ilyinalex <ilinoleksiii@gmail.com>
2018-11-13 13:05:55 +0200

    pro name is added

--- mexicanpizza.py
+++ mexicanpizza.py
@@ -4,3 +4,4 @@ import pizza
 class MexicanPizza(pizza.Pizza):
     def __init__(self, default_size, default_price, default_ingredients):
         super().__init__(default_size, default_price, default_ingredients)
+
--- order.py
+++ order.py
@@ -1,7 +1,10 @@
 class Order(object):
-    def __init__(self, pizzas):
-        self.__pizzas = pizzas
+    def __init__(self):
+        self.__ord_pizzas = {}
 
     @property
     def pizzas(self):
         return self.__pizzas
+
+    def add(self, name, ingredients):
+        self.__ord_pizzas[name] = ingredients;
